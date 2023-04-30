e6804a2ec6609591bc38bfa2fc321ca3f85ef1ed
wenqiglantz <wenqiglantz@gmail.com>
2023-04-30 08:20:59 -0400

    modified terraform code

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,40 +15,27 @@ provider "aws" {
 data "aws_caller_identity" "current" {}
 
 #######################################
-# S3 module
-#######################################
-module "s3" {
-  source         = "github.com/wenqiglantz/reusable-workflows-modules//terraform/modules/s3?ref=main"
-  aws_region     = var.aws_region
-  s3_bucket_name = var.s3_bucket_name
-  s3_object_key  = var.s3_object_key
-}
-
-#######################################
 # lambda_function
 #######################################
 module "lambda" {
   source   = "terraform-aws-modules/lambda/aws"
   for_each = var.lambda_functions
 
-  function_name       = each.key
-  runtime             = each.value.runtime
-  handler             = each.value.handler
-  create_package      = false
-  snap_start          = true
-  publish             = true
-  s3_existing_package = {
-    bucket = var.s3_bucket_name
-    key    = var.s3_object_key
-  }
+  function_name           = each.key
+  runtime                 = each.value.runtime
+  handler                 = each.value.handler
+  create_package          = false
+  snap_start              = true
+  publish                 = true
+  ignore_source_code_hash = true
 
   create_current_version_allowed_triggers = false
-#  allowed_triggers                        = {
-#    api-gateway = {
-#      service    = "apigateway"
-#      source_arn = "${aws_api_gateway_rest_api.rest_api.execution_arn}/*/*/*"
-#    }
-#  }
+  #  allowed_triggers                        = {
+  #    api-gateway = {
+  #      service    = "apigateway"
+  #      source_arn = "${aws_api_gateway_rest_api.rest_api.execution_arn}/*/*/*"
+  #    }
+  #  }
 
   environment_variables = {
     SPRING_CLOUD_FUNCTION_DEFINITION = each.value.function_name_variable
@@ -64,6 +51,7 @@ module "lambda" {
 #######################################
 module "apigatewayv2" {
   source     = "github.com/wenqiglantz/reusable-workflows-modules//terraform/modules/apigatewayv2?ref=main"
+  for_each   = var.lambda_functions
   aws_region = var.aws_region
 
   open_api_spec = templatefile("${path.root}/openai-client-openapi.json", {
@@ -75,5 +63,5 @@ module "apigatewayv2" {
   api_gateway_stage_name             = var.api_gateway_stage_name
   api_gw_log_group_retention_in_days = var.api_gw_log_group_retention_in_days
   stage_variables                    = var.stage_variables
-  lambda_function                    = element(keys(module.lambda.lambda_name), 0)
+  lambda_function                    = each.key
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,18 +3,6 @@ variable "aws_region" {
   type        = string
 }
 
-variable "s3_bucket_name" {
-  type        = string
-  description = "The name of the S3 bucket to store the Lambda function code"
-  default     = ""
-}
-
-variable "s3_object_key" {
-  type        = string
-  description = "The S3 object key, the zip file name"
-  default     = ""
-}
-
 variable "open_api_spec" {
   type        = any
   description = "OpenAPI Spec"
