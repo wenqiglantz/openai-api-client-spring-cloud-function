6cc7baec8c0d5cca7911d08a04a6179cc07e18d4
wenqiglantz <wenqiglantz@gmail.com>
2023-04-30 08:41:12 -0400

    modified terraform code

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,13 +1,3 @@
-output "lambda_name" {
-  value       = module.lambda.lambda_name
-  description = "Lambda function name"
-}
-
-output "lambda_arn" {
-  value       = module.lambda.lambda_arn
-  description = "Lambda function ARN name"
-}
-
 output "api_url" {
   value = module.apigatewayv2.base_url
 }
